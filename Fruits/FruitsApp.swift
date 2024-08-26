467e0e80ef1b9070a8bee228bbfe4e4f032c1c54
CharlesMich <charleedongo@hotmail.com>
2024-08-26 16:25:28 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Charles Michael on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
