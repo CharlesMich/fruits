6b66a49704dd5174e9830ca82ee8b6121af78425
CharlesMich <charleedongo@hotmail.com>
2024-09-24 10:10:53 -0500

    sep 24

--- Fruits/App/SettingsRowView.swift
+++ Fruits/App/SettingsRowView.swift
@@ -17,17 +17,20 @@ struct SettingsRowView: View {
     
 //    MARK: - Body
     var body: some View {
-        HStack {
-            Text(name).foregroundColor(Color.gray)
-            Spacer()
-            if (content != nil) {
-                Text(content!)
-            } else if (linkLabel != nil && linkDestination != nil) {
-                Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
-                Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
-            }
-            else {
-                EmptyView()
+        VStack {
+            Divider().padding(.vertical, 4)
+            HStack {
+                Text(name).foregroundColor(Color.gray)
+                Spacer()
+                if (content != nil) {
+                    Text(content!)
+                } else if (linkLabel != nil && linkDestination != nil) {
+                    Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+                    Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+                }
+                else {
+                    EmptyView()
+                }
             }
         }
     }
@@ -38,7 +41,7 @@ struct SettingsRowView: View {
         SettingsRowView(name: "Developer", content: "John/ Jane")
             .previewLayout(.fixed(width: 375, height: 60))
             .padding()
-        SettingsRowView(name: "Developer", content: "John / Jane")
+        SettingsRowView(name: "website", linkLabel: "swiftUI Masterclass", linkDestination: "Swiftuimasterclass.com")
             .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
             .previewLayout(.fixed(width: 375, height: 60))
             .padding()
--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
 //    MARK: - PROPERTIES
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
 //    MARK: - BODY
     var body: some View {
         NavigationView {
@@ -37,16 +38,53 @@ struct SettingsView: View {
 //                    MARK: - SECTION 2
                     
                     GroupBox(
-                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone") 
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
                         ){
                             Divider().padding(.vertical, 4)
-                            HStack {
-                                Text("Developer").foregroundColor(Color.gray)
-                                Spacer()
-                                Text("John / Jane")
+                            Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen.")
+                                .padding(.vertical, 8)
+                                .frame(minHeight: 68)
+                                .layoutPriority(1)
+                                .font(.footnote)
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                            
+                            Toggle(isOn: $isOnboarding) {
+                                if isOnboarding {
+                                    Text("Restarted".uppercased())
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .foregroundColor(Color.green)
+                                } else {
+                                    Text("Restart".uppercased())
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .foregroundColor(Color.secondary)
+                                }
                             }
+                            .padding()
+                            .background(
+                                Color(UIColor.tertiarySystemBackground)
+                                    .clipShape(RoundedRectangle(cornerRadius: 8, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                            )
                         }
-//                    MARK: - SECTION 3
+                    
+//                     MARK: - SECTION 3
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone") 
+                        ){
+                           
+                            SettingsRowView(name: "Developer", content: "John / Jane")
+                            SettingsRowView(name: "designer", content: "Robert Petras")
+                           
+                            SettingsRowView(name: "Compatibility", content: "IOS 14")
+                            SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "Swiftuimasterclass.com")
+                            SettingsRowView(name: "Twitter", linkLabel: "SwiftUI Masterclass", linkDestination: "twitter.com/robertpetras")
+                            SettingsRowView(name: "Version", content: "2.0")
+                            SettingsRowView(name: "SwiftUI", content: "1.1.0")
+                            
+                        }
+//
+                    
+                    
                 
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
